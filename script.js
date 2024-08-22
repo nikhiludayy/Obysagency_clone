6a2b7ee82de83075e3901e21bf58eca559043147
nikhiludayy <nikhiluday0823@gmail.com>
2024-08-22 12:21:06 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,34 +1,80 @@
+function loaderanimation() {
+    gsap.from("#p1 h1", {
+        y: 150,
+        opacity: 0,
+        ease: Cubic,
+        stagger: .2,
+        duration: 0.6,
+        delay: 0.5
+    })
+    gsap.from("#p2 h1", {
+        opacity: 0,
+        ease: Cubic,
+        duration: 0.8,
+        delay: 1.7
+    })
+    gsap.from(".line h5", {
+        opacity: 0,
+        delay: 1,
+        onStart: () => {
+            let gro = 0;
+            setInterval(() => {
+                if (gro > 100) {
+                    clearInterval();
+                    let tl = gsap.timeline();
+                    tl.to(".line", {
+                        opacity: 0,
+                        stagger: 0.3
+                    })
+                    tl.to("#loader", {
+                        y: "-100%",
+                        delay: .8
+                    })
+                    
 
-gsap.from(".line h5", {
-    opacity: 0,
-    delay:1,
-    onStart: () => {
-        let gro = 0;
-        setInterval(() => {
-            if (gro > 100) {
-                clearInterval();
-                loader.style.opacity = "0";
+                }
+                else { load.textContent = gro++ }
+            }, 30)
+        },
+        onComplete: () => {
+        }
+    })
 
-            }
-            else { load.textContent = gro++ }
-        }, 30)
-    }
-})
-gsap.from(".line h1", {
+
+
+    let loader = document.querySelector("#loader");
+    let load = document.querySelector(".line #loadd");
+
+}
+loaderanimation();
+
+gsap.from("#p2c h1 ", {
     y: 150,
+    ease:Cubic,
     opacity: 0,
     stagger: .2,
-    duration: 0.6,
-    delay: 0.5
-})
-gsap.from(".line h1 span", {
-    fontFamily: "plainr",
-    repeat: 10
+    duration: .8,
+    delay: 6.6
 })
 
-let loader = document.querySelector("#loader");
-let load = document.querySelector(".line #loadd");
+Shery.makeMagnet("#navprt1 h3,.menu-opener__square", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: .5,
+});
+Shery.makeMagnet("#crsr #point", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: .1,
+});
+function loadmagnet() {
+    document.addEventListener('mousemove', (del) => {
+        gsap.to("#crsr", {
+            left: del.x,
+            top: del.y,
+            duration: 0.3
+        })
 
+    })
 
-
+}
+loadmagnet();
 
